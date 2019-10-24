acb34177059bf0fd6ab9cd6c0e611e1427d9b8ee
xy <2507763874@qq.com>
2019-10-24 14:47:32 +0800

    项目修改了

---
+++ HelloGit/src/com/turing/Main.java
@@ -0,0 +1,12 @@
+package com.turing;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello Git");
+
+	    for(int i=0;i<10;i++){
+	        System.out.println("hello"+i);
+        }
+    }
+}
